be936c058f9d3452d7e95b41360244ae2ab6069d
MrN83 <31300976+MrN83@users.noreply.github.com>
2021-12-22 11:46:07 +1100

    Clean up admin.py
    Remove old/temp passwords for superuser. No longer valid.

--- sample_project/sample_app/admin.py
+++ sample_project/sample_app/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from sample_app.models import AccessRecord, Topic, Webpage, WebUser, UserProfileInfo
 
 # Register your models here. -> don't foget to create a superuser as well!
-# Superuser: name="tue" pass="pass123", email: "duckrates@gmail.com"
 admin.site.register(AccessRecord)
 admin.site.register(Topic)
 admin.site.register(Webpage)
